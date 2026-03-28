79e17ed78714742b4768b2b2159e14260b069793
Lucca <luccapertigas@gmail.com>
2026-03-28 18:00:30 -0300

    Criação do Menu

---
+++ src/main/java/br/com/ucsal/olimpiadas/Menu.java
@@ -0,0 +1,49 @@
+package br.com.ucsal.olimpiadas;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import br.com.ucsal.olimpiadas.command.Command;
+
+public class Menu {
+	private final Scanner in;
+	private final Map<String, Command> opcoes = new HashMap<>();
+	
+	public Menu(Scanner in) {
+			this.in = in;
+	}
+	
+	public void registrar(String tecla, Command command) {
+		opcoes.put(tecla, command);
+	}
+	
+	public void iniciar() {
+		exibirMenu();
+		
+		String opcao = in.nextLine();
+		
+		Command command = opcoes.get(opcao);
+		
+		if (command != null) {
+			command.executar();
+		} else {
+			opcaoInvalida();
+		}
+	}
+	
+	public void exibirMenu() {
+		System.out.println("\n=== OLIMPÍADA DE QUESTÕES (V1) ===");
+		System.out.println("1) Cadastrar participante");
+		System.out.println("2) Cadastrar prova");
+		System.out.println("3) Cadastrar questão (A–E) em uma prova");
+		System.out.println("4) Aplicar prova (selecionar participante + prova)");
+		System.out.println("5) Listar tentativas (resumo)");
+		System.out.println("0) Sair");
+		System.out.print("> ");
+	}
+	
+	public void opcaoInvalida() {
+		System.out.println("Opcao invalida, tente novamente !");
+	}
+}
